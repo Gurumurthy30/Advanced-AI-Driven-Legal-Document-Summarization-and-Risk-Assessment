52c51425e20cf0605c1017f44db14f0b50f7fbc3
Gurumurthy30 <gurumurthy.00300@gmail.com>
2025-02-22 22:27:24 +0530

    pdf loader

--- Image_to_text.py
+++ Image_to_text.py
@@ -1,18 +1,38 @@
 import cv2
 import pytesseract
+import os
+from dotenv import load_dotenv
+import fitz
+from PIL import Image
+import io
 
-# Specify the Tesseract OCR path
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image_path = "C:\\Users\\gurum\\OneDrive\\Pictures\\Screenshots\\Screenshot 2025-02-15 132040.png"
+def Extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    full_text = []
 
-image = cv2.imread(image_path)
+    for page_num in range(len(doc)):
+        page  = doc[page_num]
+        text = page.get_text("text")
 
-# Debugging print
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        if text.strip():
+            full_text.append(text)
+        else:
+            image_list = page.get_images(page)
+            extracted_text=""
 
-# Extract text
-text = pytesseract.image_to_string(gray)
+            for img_index,img in enumerate(image_list):
+                xref = img[0]
+                base_image = doc.extract_image(xref)
+                image_bytes = base_image["image"]
+                img_pil = Image.open(io.BytesIO(image_bytes))
+                extracted_text+=pytesseract.image_to_string(img_pil) + "/n"
+                full_text.append(extracted_text)
+    return("\n".join(full_text))
 
-# Print the extracted text
-print(text)
+
+#doc = r"C:\Users\gurum\Documents\2025021580.pdf"
+#extract_text = Extract_text_from_pdf(doc)
+#lines = extract_text.split("\n")
+#print(extract_text)
